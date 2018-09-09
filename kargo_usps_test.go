aa82490558fe94ce3993e9d8db64d2fe04867619
Bennett Mobley <mobley.bennett@gmail.com>
2018-09-09 10:46:23 -0500

    Initial tests

--- kargo_usps_test.go
+++ kargo_usps_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestUSPSPackageMatch(t *testing.T) {
-	p, _ := NewPackage("1Z999AA10123456784")
+	p, _ := NewPackage("9400 1000 1337 0000 0000 00")
 	usps := NewUSPS(p)
 	if usps.Match() != true {
 		t.Errorf("Failed, expected: %t, want: %t.", true, usps.Match())
@@ -22,7 +22,7 @@ func TestUSPSPackageWithNonValidNumber(t *testing.T) {
 }
 
 func TestUSPSPackageStringCheckDigit(t *testing.T) {
-	p, _ := NewPackage("1Z999AA1012345678s")
+	p, _ := NewPackage("9400 1000 leet 0000 0000 00")
 	usps := NewUSPS(p)
 	usps.Validate()
 	if usps.Package.IsValid != false {
@@ -31,16 +31,7 @@ func TestUSPSPackageStringCheckDigit(t *testing.T) {
 }
 
 func TestUSPSPackageIsValid(t *testing.T) {
-	p, _ := NewPackage("1Z999AA10123456784")
-	usps := NewUSPS(p)
-	usps.Validate()
-	if usps.Package.IsValid != true {
-		t.Errorf("Failed, expected: %t, want: %t.", true, usps.Package.IsValid)
-	}
-}
-
-func TestUSPSValidateZeroChecksum(t *testing.T) {
-	p, _ := NewPackage("1Z999AA10123456784")
+	p, _ := NewPackage("9400 1000 1337 0000 0000 00")
 	usps := NewUSPS(p)
 	usps.Validate()
 	if usps.Package.IsValid != true {
