60db45ad2b4e8114e4b4ddb835f1e4bbdde4c4c8
cnone <sinanulker386@gmail.com>
2018-10-14 00:56:46 +0300

    Add tests for usps

--- kargo_usps_test.go
+++ kargo_usps_test.go
@@ -4,37 +4,136 @@ import (
 	"testing"
 )
 
-func TestUSPSPackageMatch(t *testing.T) {
-	p, _ := NewPackage("9400 1000 1337 0000 0000 00")
-	usps := NewUSPS(p)
+// Tests for usps IMpb
+func TestUSPSIMpbPackageMatch(t *testing.T) {
+	p, _ := NewPackage("420221539101026837331000039521")
+	usps := NewUSPSIMpb(p)
 	if usps.Match() != true {
-		t.Errorf("Failed, expected: %t, want: %t.", true, usps.Match())
+		t.Errorf("Failed, expected: %t, have: %t.", true, usps.Match())
 	}
 }
 
-func TestUSPSPackageWithNonValidNumber(t *testing.T) {
+func TestUSPSIMpbPackageValidNoZip(t *testing.T) {
+	p, _ := NewPackage("9212391234567812345670")
+	usps := NewUSPSIMpb(p)
+	if usps.Match() != true {
+		t.Errorf("Failed, expected: %t, have: %t.", false, usps.Package.IsValid)
+	}
+}
+
+func TestUSPSIMpbPackageValidNineDigitZip(t *testing.T) {
+	p, _ := NewPackage("4209731792009205592767756015842558")
+	usps := NewUSPSIMpb(p)
+	if usps.Match() != true {
+		t.Errorf("Failed, expected: %t, have: %t.", false, usps.Package.IsValid)
+	}
+}
+
+func TestUSPSIMpbPackageWithNonValidNumber(t *testing.T) {
 	p, _ := NewPackage("1Z999AA10723456784")
-	usps := NewUSPS(p)
+	usps := NewUSPSIMpb(p)
 	usps.Validate()
 	if usps.Package.IsValid != false {
-		t.Errorf("Failed, expected: %t, want: %t.", false, usps.Package.IsValid)
+		t.Errorf("Failed, expected: %t, have: %t.", false, usps.Package.IsValid)
 	}
 }
 
-func TestUSPSPackageStringCheckDigit(t *testing.T) {
-	p, _ := NewPackage("9400 1000 leet 0000 0000 00")
-	usps := NewUSPS(p)
-	usps.Validate()
-	if usps.Package.IsValid != false {
-		t.Errorf("Failed, expected: %t, want: %t.", true, usps.Package.IsValid)
-	}
-}
-
-func TestUSPSPackageIsValid(t *testing.T) {
-	p, _ := NewPackage("9400 1000 1337 0000 0000 00")
-	usps := NewUSPS(p)
+func TestUSPSIMpbPackageZeroCheckDigit(t *testing.T) {
+	p, _ := NewPackage("9212391234567812345670")
+	usps := NewUSPSIMpb(p)
 	usps.Validate()
 	if usps.Package.IsValid != true {
-		t.Errorf("Failed, expected: %t, want: %t.", true, usps.Package.IsValid)
+		t.Errorf("Failed, expected: %t, have: %t.", true, usps.Package.IsValid)
+	}
+}
+
+func TestUSPSIMpbPackageIsValid(t *testing.T) {
+	expected_value := "9101026837331000039521"
+	p, _ := NewPackage("420221539101026837331000039521")
+	usps := NewUSPSIMpb(p)
+	usps.Validate()
+	if usps.Package.TrackingNumber != expected_value {
+		t.Errorf("Failed, expected: %v, have: %v.",
+			expected_value, usps.Package.TrackingNumber)
+	}
+}
+
+// Tests for usps s10
+func TestUSPSS10PackageMatch(t *testing.T) {
+	p, _ := NewPackage("EF123456785US")
+	usps := NewUSPSS10(p)
+	if usps.Match() != true {
+		t.Errorf("Failed, expected: %t, have: %t.", true, usps.Match())
+	}
+}
+
+func TestUSPSS10PackageWithNonValidNumber(t *testing.T) {
+	p, _ := NewPackage("EF12345678US") // 8 digit
+	usps := NewUSPSS10(p)
+	usps.Validate()
+	if usps.Package.IsValid != false {
+		t.Errorf("Failed, expected: %t, have: %t.", false, usps.Package.IsValid)
+	}
+}
+
+func TestUSPSS10PackageValidCheckDigit(t *testing.T) {
+	p, _ := NewPackage("RZ030057180PH")
+	usps := NewUSPSS10(p)
+	usps.Validate()
+	if usps.Package.IsValid != true {
+		t.Errorf("Failed, expected: %t, have: %t.", true, usps.Package.IsValid)
+	}
+}
+func TestUSPSS10PackageValidCheckDigitRemainder11(t *testing.T) {
+	p, _ := NewPackage("VA456789015KG")
+	usps := NewUSPSS10(p)
+	usps.Validate()
+	if usps.Package.IsValid != true {
+		t.Errorf("Failed, expected: %t, have: %t.", true, usps.Package.IsValid)
+	}
+}
+
+func TestUSPSS10PackageIsValid(t *testing.T) {
+	p, _ := NewPackage("EF123456785US")
+	usps := NewUSPSS10(p)
+	usps.Validate()
+	if usps.Package.IsValid != true {
+		t.Errorf("Failed, expected: %t, have: %t.", true, usps.Package.IsValid)
+	}
+}
+
+// Tests for usps 20
+func TestUSPS20PackageMatch(t *testing.T) {
+	p, _ := NewPackage("71123456789123456787")
+	usps := NewUSPS20(p)
+	if usps.Match() != true {
+		t.Errorf("Failed, expected: %t, have: %t.", true, usps.Match())
+	}
+}
+
+func TestUSPS20PackageWithNonValidNumber(t *testing.T) {
+	p, _ := NewPackage("72123456789123456787")
+	usps := NewUSPS20(p)
+	usps.Validate()
+	if usps.Package.IsValid != false {
+		t.Errorf("Failed, expected: %t, have: %t.", false, usps.Package.IsValid)
+	}
+}
+
+func TestUSPS20PackageWrongCheckDigit(t *testing.T) {
+	p, _ := NewPackage("7112345678912345678r")
+	usps := NewUSPS20(p)
+	usps.Validate()
+	if usps.Package.IsValid != false {
+		t.Errorf("Failed, expected: %t, have: %t.", true, usps.Package.IsValid)
+	}
+}
+
+func TestUSPS20PackageIsValid(t *testing.T) {
+	p, _ := NewPackage("71123456789123456787")
+	usps := NewUSPS20(p)
+	usps.Validate()
+	if usps.Package.IsValid != true {
+		t.Errorf("Failed, expected: %t, have: %t.", true, usps.Package.IsValid)
 	}
 }
