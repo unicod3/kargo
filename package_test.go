5b794e9d9b906757e96fb64dc99e7ee3163bdfff
cnone <sinanulker386@gmail.com>
2018-10-14 01:21:52 +0300

    Combine stringutil into package

--- package_test.go
+++ package_test.go
@@ -21,3 +21,18 @@ func TestPackageTrackingNumberWithSpaces(t *testing.T) {
 		t.Errorf("Failed, expected: %v, got: %v.", expectedValue, p.TrackingNumber)
 	}
 }
+
+func TestReverse(t *testing.T) {
+	for _, c := range []struct {
+		in, want string
+	}{
+		{"Hello, world", "dlrow ,olleH"},
+		{"Hello, 世界", "界世 ,olleH"},
+		{"", ""},
+	} {
+		got := Reverse(c.in)
+		if got != c.want {
+			t.Errorf("Reverse(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
