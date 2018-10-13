503e7f71b527c06032e8d8f09119f6977c3fba6b
cnone <sinanulker386@gmail.com>
2018-10-14 02:21:02 +0300

    Refactor usps validation tests

--- usps_test.go
+++ usps_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-// Tests for usps IMpb
+// Tests for USPSIMpb
 func TestUSPSIMpbPackageMatch(t *testing.T) {
 	p, _ := NewPackage("420221539101026837331000039521")
 	usps := NewUSPSIMpb(p)
@@ -13,59 +13,27 @@ func TestUSPSIMpbPackageMatch(t *testing.T) {
 	}
 }
 
-func TestUSPSIMpbPackageValidNoZip(t *testing.T) {
-	p, _ := NewPackage("9212391234567812345670")
-	usps := NewUSPSIMpb(p)
-	if usps.Match() != true {
-		t.Errorf("Failed, expected: %t, have: %t.", false, usps.Package.IsValid)
+func TestUSPSIMpbPackageValidate(t *testing.T) {
+	trackingNumbers := map[string]bool{
+		"9212391234567812745670":             false, // Non Valid Tracking Number
+		"420973179200920559276775601584255r": false, // Char Check Digit
+		"420973179200r205592767756015842559": false, // Have a Char
+		"4209731792009205592767756015842559": false, // Non Valid Check Digit
+		"4209731792009205592767756015842558": true,  // Valid Nine Digit Zip
+		"9212391234567812345670":             true,  // Valid Zero Check Digit
+	}
+
+	for trackingNumber, expected := range trackingNumbers {
+		p, _ := NewPackage(trackingNumber)
+		u := NewUSPSIMpb(p)
+		u.Validate()
+		if u.Package.IsValid != expected {
+			t.Errorf("Failed: %s, expected: %t, have: %t.", trackingNumber, expected, u.Package.IsValid)
+		}
 	}
 }
 
-func TestUSPSIMpbPackageValidNineDigitZip(t *testing.T) {
-	p, _ := NewPackage("4209731792009205592767756015842558")
-	usps := NewUSPSIMpb(p)
-	if usps.Match() != true {
-		t.Errorf("Failed, expected: %t, have: %t.", false, usps.Package.IsValid)
-	}
-}
-
-func TestUSPSIMpbPackageWithNonValidNumber(t *testing.T) {
-	p, _ := NewPackage("1Z999AA10723456784")
-	usps := NewUSPSIMpb(p)
-	usps.Validate()
-	if usps.Package.IsValid != false {
-		t.Errorf("Failed, expected: %t, have: %t.", false, usps.Package.IsValid)
-	}
-}
-
-func TestUSPSIMpbPackageWithNonValidCharCheckDigit(t *testing.T) {
-	p, _ := NewPackage("420973179200920559276775601584255r")
-	usps := NewUSPSIMpb(p)
-	usps.Validate()
-	if usps.Package.IsValid != false {
-		t.Errorf("Failed, expected: %t, have: %t.", false, usps.Package.IsValid)
-	}
-}
-
-func TestUSPSIMpbPackageWithNonValidCheckDigit(t *testing.T) {
-	p, _ := NewPackage("4209731792009205592767756015842559")
-	usps := NewUSPSIMpb(p)
-	usps.Validate()
-	if usps.Package.IsValid != false {
-		t.Errorf("Failed, expected: %t, have: %t.", false, usps.Package.IsValid)
-	}
-}
-
-func TestUSPSIMpbPackageZeroCheckDigit(t *testing.T) {
-	p, _ := NewPackage("9212391234567812345670")
-	usps := NewUSPSIMpb(p)
-	usps.Validate()
-	if usps.Package.IsValid != true {
-		t.Errorf("Failed, expected: %t, have: %t.", true, usps.Package.IsValid)
-	}
-}
-
-func TestUSPSIMpbPackageIsValid(t *testing.T) {
+func TestUSPSIMpbTrackingNumberFormatting(t *testing.T) {
 	expected_value := "9101026837331000039521"
 	p, _ := NewPackage("420221539101026837331000039521")
 	usps := NewUSPSIMpb(p)
@@ -76,7 +44,7 @@ func TestUSPSIMpbPackageIsValid(t *testing.T) {
 	}
 }
 
-// Tests for usps s10
+// Tests for USPSS10
 func TestUSPSS10PackageMatch(t *testing.T) {
 	p, _ := NewPackage("EF123456785US")
 	usps := NewUSPSS10(p)
@@ -85,59 +53,27 @@ func TestUSPSS10PackageMatch(t *testing.T) {
 	}
 }
 
-func TestUSPSS10PackageWithNonValidChar(t *testing.T) {
-	p, _ := NewPackage("EF12r456786US")
-	usps := NewUSPSS10(p)
-	usps.Validate()
-	if usps.Package.IsValid != false {
-		t.Errorf("Failed, expected: %t, have: %t.", false, usps.Package.IsValid)
+func TestUSPSS10PackageValidate(t *testing.T) {
+	trackingNumbers := map[string]bool{
+		"EF12345678US":  false, // Non Valid Tracking Number
+		"EF12r456786US": false, // Have a Char
+		"EF123456786US": false, // Non Valid Check Digit
+		"EF123456785US": true,  // Valid Tracking Number
+		"RZ030057180PH": true,  // Valid Check Digit
+		"VA456789015KG": true,  // Valid Check Digit With Remainder 11
+	}
+
+	for trackingNumber, expected := range trackingNumbers {
+		p, _ := NewPackage(trackingNumber)
+		u := NewUSPSS10(p)
+		u.Validate()
+		if u.Package.IsValid != expected {
+			t.Errorf("Failed: %s, expected: %t, have: %t.", trackingNumber, expected, u.Package.IsValid)
+		}
 	}
 }
 
-func TestUSPSS10PackageWithNonValidCheckDigit(t *testing.T) {
-	p, _ := NewPackage("EF123456786US")
-	usps := NewUSPSS10(p)
-	usps.Validate()
-	if usps.Package.IsValid != false {
-		t.Errorf("Failed, expected: %t, have: %t.", false, usps.Package.IsValid)
-	}
-}
-func TestUSPSS10PackageWithNonValidNumber(t *testing.T) {
-	p, _ := NewPackage("EF12345678US") // 8 digit
-	usps := NewUSPSS10(p)
-	usps.Validate()
-	if usps.Package.IsValid != false {
-		t.Errorf("Failed, expected: %t, have: %t.", false, usps.Package.IsValid)
-	}
-}
-
-func TestUSPSS10PackageValidCheckDigit(t *testing.T) {
-	p, _ := NewPackage("RZ030057180PH")
-	usps := NewUSPSS10(p)
-	usps.Validate()
-	if usps.Package.IsValid != true {
-		t.Errorf("Failed, expected: %t, have: %t.", true, usps.Package.IsValid)
-	}
-}
-func TestUSPSS10PackageValidCheckDigitRemainder11(t *testing.T) {
-	p, _ := NewPackage("VA456789015KG")
-	usps := NewUSPSS10(p)
-	usps.Validate()
-	if usps.Package.IsValid != true {
-		t.Errorf("Failed, expected: %t, have: %t.", true, usps.Package.IsValid)
-	}
-}
-
-func TestUSPSS10PackageIsValid(t *testing.T) {
-	p, _ := NewPackage("EF123456785US")
-	usps := NewUSPSS10(p)
-	usps.Validate()
-	if usps.Package.IsValid != true {
-		t.Errorf("Failed, expected: %t, have: %t.", true, usps.Package.IsValid)
-	}
-}
-
-// Tests for usps 20
+// Tests for USPS20
 func TestUSPS20PackageMatch(t *testing.T) {
 	p, _ := NewPackage("71123456789123456787")
 	usps := NewUSPS20(p)
@@ -146,37 +82,20 @@ func TestUSPS20PackageMatch(t *testing.T) {
 	}
 }
 
-func TestUSPS20PackageWithNonValidNumber(t *testing.T) {
-	p, _ := NewPackage("72123456789123456787")
-	usps := NewUSPS20(p)
-	usps.Validate()
-	if usps.Package.IsValid != false {
-		t.Errorf("Failed, expected: %t, have: %t.", false, usps.Package.IsValid)
+func TestUSPS20PackageValidate(t *testing.T) {
+	trackingNumbers := map[string]bool{
+		"72123456789123456787": false, // Non Valid Tracking Number
+		"71123456r89123456787": false, // Have a Char
+		"7112345678912345678r": false, // Char Check Digit
+		"71123456789123456787": true,  // Valid Tracking Number
 	}
-}
 
-func TestUSPS20PackageWithNonValidChar(t *testing.T) {
-	p, _ := NewPackage("71123456r89123456787")
-	usps := NewUSPS20(p)
-	usps.Validate()
-	if usps.Package.IsValid != false {
-		t.Errorf("Failed, expected: %t, have: %t.", true, usps.Package.IsValid)
-	}
-}
-func TestUSPS20PackageWrongCheckDigit(t *testing.T) {
-	p, _ := NewPackage("7112345678912345678r")
-	usps := NewUSPS20(p)
-	usps.Validate()
-	if usps.Package.IsValid != false {
-		t.Errorf("Failed, expected: %t, have: %t.", true, usps.Package.IsValid)
-	}
-}
-
-func TestUSPS20PackageIsValid(t *testing.T) {
-	p, _ := NewPackage("71123456789123456787")
-	usps := NewUSPS20(p)
-	usps.Validate()
-	if usps.Package.IsValid != true {
-		t.Errorf("Failed, expected: %t, have: %t.", true, usps.Package.IsValid)
+	for trackingNumber, expected := range trackingNumbers {
+		p, _ := NewPackage(trackingNumber)
+		u := NewUSPS20(p)
+		u.Validate()
+		if u.Package.IsValid != expected {
+			t.Errorf("Failed: %s, expected: %t, have: %t.", trackingNumber, expected, u.Package.IsValid)
+		}
 	}
 }
