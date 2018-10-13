ed86f7f792a418449b36829b3f1847a62804f25b
cnone <sinanulker386@gmail.com>
2018-10-14 01:59:04 +0300

    Refactor ups validation tests

--- ups_test.go
+++ ups_test.go
@@ -8,42 +8,24 @@ func TestUPSPackageMatch(t *testing.T) {
 	p, _ := NewPackage("1Z999AA10123456784")
 	ups := NewUPS(p)
 	if ups.Match() != true {
-		t.Errorf("Failed, expected: %t, want: %t.", true, ups.Match())
+		t.Errorf("Failed, expected: %t, have: %t.", true, ups.Match())
 	}
 }
 
-func TestUPSPackageWithNonValidNumber(t *testing.T) {
-	p, _ := NewPackage("1Z999AA10723456784")
-	ups := NewUPS(p)
-	ups.Validate()
-	if ups.Package.IsValid != false {
-		t.Errorf("Failed, expected: %t, want: %t.", false, ups.Package.IsValid)
+func TestUPSPackageValidate(t *testing.T) {
+	trackingNumbers := map[string]bool{
+		"1Z999AA10723456784": false, // Non Valid Tracking Number
+		"1Z999AA1012345678s": false, // Char Check Digit
+		"1Z999AA10123456784": true,  // Valid Tracking Number
+		"1Z879E930346834440": true,  // Valid Zero Check Digit
 	}
-}
 
-func TestUPSPackageStringCheckDigit(t *testing.T) {
-	p, _ := NewPackage("1Z999AA1012345678s")
-	ups := NewUPS(p)
-	ups.Validate()
-	if ups.Package.IsValid != false {
-		t.Errorf("Failed, expected: %t, want: %t.", true, ups.Package.IsValid)
-	}
-}
-
-func TestUPSPackageIsValid(t *testing.T) {
-	p, _ := NewPackage("1Z999AA10123456784")
-	ups := NewUPS(p)
-	ups.Validate()
-	if ups.Package.IsValid != true {
-		t.Errorf("Failed, expected: %t, want: %t.", true, ups.Package.IsValid)
-	}
-}
-
-func TestUPSValidateZeroChecksum(t *testing.T) {
-	p, _ := NewPackage("1Z999AA10123456784")
-	ups := NewUPS(p)
-	ups.Validate()
-	if ups.Package.IsValid != true {
-		t.Errorf("Failed, expected: %t, want: %t.", true, ups.Package.IsValid)
+	for trackingNumber, expected := range trackingNumbers {
+		p, _ := NewPackage(trackingNumber)
+		ups := NewUPS(p)
+		ups.Validate()
+		if ups.Package.IsValid != expected {
+			t.Errorf("Failed, expected: %t, have: %t.", expected, ups.Package.IsValid)
+		}
 	}
 }
