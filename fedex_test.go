11fb4e6ed45d28d6a00b610efb48b9592cf14d76
cnone <sinanulker386@gmail.com>
2018-10-14 02:33:30 +0300

    Refactor fedEx tests

--- fedex_test.go
+++ fedex_test.go
@@ -12,19 +12,20 @@ func TestFedExExpressPackageMatch(t *testing.T) {
 	}
 }
 
-func TestFedExExpressPackage(t *testing.T) {
-	trackingNumbers := [3]string {
-		"9632001960000000000400152152192158", //WithNonValidNumber
-		"96320019600000000004001521s2152151", //StringDigit
-		"963200196000000000040015215215215s", //StringCheckDigit
+func TestFedExExpressPackageValidate(t *testing.T) {
+	trackingNumbers := map[string]bool{
+		"9632001960000000000400152152192158": false, // Not Valid Tracking Number
+		"96320019600000000004001521s2152151": false, // Have Char Digit
+		"963200196000000000040015215215215s": false, // Char Check Digit
+		"9632001960000000000400152152152158": true,  // Valid Tracking Number
 	}
 
-	for _, trackingNumber := range trackingNumbers {
+	for trackingNumber, expected := range trackingNumbers {
 		p, _ := NewPackage(trackingNumber)
 		fexpress := NewFedExExpress(p)
 		fexpress.Validate()
-		if fexpress.Package.IsValid {
-			t.Errorf("Failed for %s, expected: %t, got: %t.", trackingNumber, false, fexpress.Package.IsValid)
+		if fexpress.Package.IsValid != expected {
+			t.Errorf("Failed: %s, expected: %t, have: %t.", trackingNumber, expected, fexpress.Package.IsValid)
 		}
 	}
 }
@@ -39,15 +40,6 @@ func TestFedExExpressPackageTrackingNumber(t *testing.T) {
 	}
 }
 
-func TestFedExExpressCarrier(t *testing.T) {
-	p, _ := NewPackage("9632001960000000000400152152152158")
-	fexpress := NewFedExExpress(p)
-	fexpress.Validate()
-	if fexpress.Package.Carrier != fexpress.GetCarrierName() {
-		t.Errorf("Failed, expected: %v, got: %v.", fexpress.GetCarrierName(), fexpress.Package.Carrier)
-	}
-}
-
 func TestFedExGround96PackageMatch(t *testing.T) {
 	p, _ := NewPackage("9611019012345612345671")
 	fground := NewFedExGround96(p)
@@ -57,28 +49,21 @@ func TestFedExGround96PackageMatch(t *testing.T) {
 }
 
 func TestFedExGround96Package(t *testing.T) {
-	trackingNumbers := [3]string {
-		"9611019012345612945671", //WithNonValidNumber
-		"96110190123456s2345671", //StringDigit
-		"961101901234561234567s", //StringCheckDigit
+	trackingNumbers := map[string]bool{
+		"9611019012345612945671": false, // Not Valid Tracking Number
+		"96110190123456s2345671": false, // Have a Char Digit
+		"961101901234561234567s": false, // Char Check Digit
+		"9611019012345612345671": true,  // Valid Tracking Number
+		"9611019012345612345640": true,  // Zero Check Digit
 	}
 
-	for _, trackingNumber := range trackingNumbers {
+	for trackingNumber, expected := range trackingNumbers {
 		p, _ := NewPackage(trackingNumber)
 		fground := NewFedExGround96(p)
 		fground.Validate()
-		if fground.Package.IsValid {
-			t.Errorf("Failed for %s, expected: %t, got: %t.", trackingNumber, false, fground.Package.IsValid)
+		if fground.Package.IsValid != expected {
+			t.Errorf("Failed: %s, expected: %t, have: %t.", trackingNumber, expected, fground.Package.IsValid)
 		}
-	}
-}
-
-func TestFedExGround96PackageIsValid(t *testing.T) {
-	p, _ := NewPackage("9611019012345612345671")
-	fground := NewFedExGround96(p)
-	fground.Validate()
-	if !fground.Package.IsValid {
-		t.Errorf("Failed, expected: %t, got: %t.", true, fground.Package.IsValid)
 	}
 }
 
@@ -89,21 +74,5 @@ func TestFedExGround96PackageTrackingNumber(t *testing.T) {
 	fground.Validate()
 	if fground.Package.TrackingNumber != expectedValue {
 		t.Errorf("Failed, expected: %v, got: %v.", expectedValue, fground.Package.TrackingNumber)
-	}
-}
-
-func TestFedExGround96(t *testing.T) {
-	trackingNumbers := [2]string {
-		"9611019012345612345640", //ZeroChecksum
-		"9611019012345612345671", //Carrier
-	}
-
-	for _, trackingNumber := range trackingNumbers {
-		p, _ := NewPackage(trackingNumber)
-		fground := NewFedExGround96(p)
-		fground.Validate()
-		if fground.Package.Carrier != fground.GetCarrierName() {
-			t.Errorf("Failed, expected: %v, got: %v.", fground.GetCarrierName(), fground.Package.Carrier)
-		}
 	}
 }
